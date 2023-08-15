79cd1859094fc656cf5621ca47c934aa702e48e5
Orkun AVCI <orkunavci28@gmail.com>
2023-08-15 18:26:41 +0300

    Create definitions.py

---
+++ Game/master/definitions.py
@@ -0,0 +1,145 @@
+workers = [
+	{
+		"id": 0,
+		"name": "Newbie",
+		"cost": 10000,
+		"production": 1,
+		"level": 0,
+	},
+	{
+		"id": 1,
+		"name": "Worker",
+		"cost": 25000,
+		"production": 5,
+		"level": 3,
+	},
+	{
+		"id": 2,
+		"name": "Professional",
+		"cost": 100000,
+		"production": 25,
+		"level": 7,
+	},
+	{
+		"id": 3,
+		"name": "Manager",
+		"cost": 250000,
+		"production": 70,
+		"level": 10,
+	},
+	{
+		"id": 4,
+		"name": "CEO",
+		"cost": 500000,
+		"production": 250,
+		"level": 12,
+	},
+]
+
+storages = [
+	{
+		"id": 0,
+		"name": "Garage",
+		"cost": 20000,
+		"storage": 500,
+		"level": 1,
+	},
+	{
+		"id": 1,
+		"name": "Small Depot",
+		"cost": 40000,
+		"storage": 2000,
+		"level": 4,
+	},
+	{
+		"id": 2,
+		"name": "Medium Depot",
+		"cost": 80000,
+		"storage": 6000,
+		"level": 7,
+	},
+	{
+		"id": 3,
+		"name": "Large Depot",
+		"cost": 150000,
+		"storage": 20000,
+		"level": 10,
+	},
+	{
+		"id": 4,
+		"name": "Mega Depot",
+		"cost": 300000,
+		"storage": 60000,
+		"level": 12,
+	},
+]
+
+reputations = [
+	{
+		"id": 0,
+		"name": "Van",
+		"cost": 25000,
+		"rep": 100,
+	},
+	{
+		"id": 1,
+		"name": "Small Car",
+		"cost": 50000,
+		"rep": 300,
+	},
+	{
+		"id": 2,
+		"name": "Work Car",
+		"cost": 150000,
+		"rep": 1200,
+	},
+	{
+		"id": 3,
+		"name": "Lux Car",
+		"cost": 300000,
+		"rep": 3000,
+	},
+	{
+		"id": 4,
+		"name": "Sports Car",
+		"cost": 600000,
+		"rep": 7500,
+	},
+]
+
+level_req = [
+	1000,
+	2500,
+	5000,
+	8000,
+	12000,
+	16500,
+	22500,
+	30000,
+	45000,
+	60000,
+	75000,
+	95000,
+	125000,
+	160000,
+	200000,
+    100000000000000000000000000
+]
+
+selling_mult = [
+	8,
+	9,
+	10,
+	12,
+	14,
+	16,
+	19,
+	21,
+	24,
+	26,
+	30,
+	32,
+	35,
+	40,
+	50
+]
